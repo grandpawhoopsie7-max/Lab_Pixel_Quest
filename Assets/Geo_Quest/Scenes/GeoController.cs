50b44a0bd758a59de93a0433bf9bab3c1b481cd4
grandpawhoopsie7-max <grandpa.whoopsie7@gmail.com>
2026-02-14 16:32:04 -0500

    CREATED GOATED GAME
    DAP ME UP MY NEIGHBOR

---
+++ Assets/Geo_Quest/Scenes/GeoController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeoController : MonoBehaviour {
+    string playerName = "Hello";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello World");
+        string playerName2 = "World";
+        Debug.Log(playerName + playerName2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
