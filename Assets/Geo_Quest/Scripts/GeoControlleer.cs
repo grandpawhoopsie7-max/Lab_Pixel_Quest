e4cc2491eaa4e732bf4d5acb41157e12ced4916b
Sebastian Grygorczuk <stem08992@gmail.com>
2026-02-18 09:04:59 -0500

    2/14

---
+++ Assets/Geo_Quest/Scripts/GeoControlleer.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeoControlleer : MonoBehaviour {
+    int counter = 0;
+    
+    // Start is called before the first frame update
+    void Start() {
+        counter = 1;
+        int highscore = 2; 
+        Debug.Log("hasdasd");
+    }
+
+    // Update is called once per frame
+    void Update() {
+        highscore = 2;
+        counter = 2; 
+    }
+
+}
