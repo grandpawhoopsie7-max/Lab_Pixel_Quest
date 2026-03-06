9b4c07272d75ef5c4f5107f004e9977012477843
grandpawhoopsie7-max <grandpa.whoopsie7@gmail.com>
2026-03-05 23:41:52 -0500

    HW2 #2

---
+++ Assets/Hands-On Homework #2/Scripts/BigRightBullet.cs
@@ -0,0 +1,64 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerShoot : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    public Transform bulletSpawn;
+    public Transform bulletTrash;
+
+    public float bulletSpeed = 20f;
+    public float bulletSize = 2f;
+    public Color bulletColor = Color.red;
+
+    public float shootCooldown = 0.5f;
+    private float currentCooldown = 0f;
+
+    public Image cooldownBar;
+
+    void Update()
+    {
+        // Cooldown timer
+        if (currentCooldown > 0)
+        {
+            currentCooldown -= Time.deltaTime;
+        }
+
+        // Update cooldown bar
+        if (cooldownBar != null)
+        {
+            cooldownBar.fillAmount = 1 - (currentCooldown / shootCooldown);
+        }
+
+        // Right mouse button shoot
+        if (Input.GetMouseButtonDown(1) && currentCooldown <= 0)
+        {
+            Shoot();
+            currentCooldown = shootCooldown;
+        }
+    }
+
+    void Shoot()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, bulletSpawn.position, transform.rotation);
+
+        bullet.transform.SetParent(bulletTrash);
+
+        // Make bullet bigger
+        bullet.transform.localScale *= bulletSize;
+
+        // Change bullet color
+        SpriteRenderer sr = bullet.GetComponent<SpriteRenderer>();
+        if (sr != null)
+        {
+            sr.color = bulletColor;
+        }
+
+        // Make bullet move faster
+        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+        if (rb != null)
+        {
+            rb.linearVelocity = transform.up * bulletSpeed;
+        }
+    }
+}
--- Assets/Hands-On Homework #2/Scripts/HW2PlayerDie.cs
+++ Assets/Hands-On Homework #2/Scripts/HW2PlayerDie.cs
@@ -7,7 +7,7 @@ public class HW2PlayerDie : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.tag == "Enemy")
+        if(collision.gameObject.tag == Enemy)
         {
             endPanel.SetActive(true);
             gameObject.SetActive(false);
